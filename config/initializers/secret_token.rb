f326eedc150674b4a605c797b01c80cf5017ceb4
Mitch Yarchin <mitch@Mitch-Yarchins-MacBook-Pro.local>
2014-04-22 13:47:46 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DREmailer::Application.config.secret_key_base = 'b2b86b4e3b11b5605227095f93742c1320657390460b0a55d68e9021ac0c7ca3a50c5ab04221d997bb548de2bc1e6a05943a18b652d9e61143b24b7d77e41700'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DREmailer::Application.config.session_store :cookie_store, key: '_DR-emailer_session'
